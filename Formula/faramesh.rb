54e93c52166c4d4037d4e903f95b34963550c520
amjad-fatmi <mjaad7z@proton.me>
2026-03-22 02:43:55 -0400

    fix(formula): correct escaped interpolation in URLs

--- Formula/faramesh.rb
+++ Formula/faramesh.rb
@@ -6,22 +6,22 @@ class Faramesh < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/faramesh/faramesh-core/releases/download/v\#{version}/faramesh-darwin-arm64"
+      url "https://github.com/faramesh/faramesh-core/releases/download/v#{version}/faramesh-darwin-arm64"
       sha256 "df9c6b03ee5d67a5549d6ab2966e8b7cf8960bb028a28973ded25c829e3ce06b"
     end
     on_intel do
-      url "https://github.com/faramesh/faramesh-core/releases/download/v\#{version}/faramesh-darwin-amd64"
+      url "https://github.com/faramesh/faramesh-core/releases/download/v#{version}/faramesh-darwin-amd64"
       sha256 "2c746ab9e2dfe335e0d8cdd7338a765fb311fd165c7268af6ee209ddc90a3f16"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/faramesh/faramesh-core/releases/download/v\#{version}/faramesh-linux-arm64"
+      url "https://github.com/faramesh/faramesh-core/releases/download/v#{version}/faramesh-linux-arm64"
       sha256 "622e99fbc2d427f7137b3e6d0192c780b2abf2c5eb6628dc6b1842b54edbcafe"
     end
     on_intel do
-      url "https://github.com/faramesh/faramesh-core/releases/download/v\#{version}/faramesh-linux-amd64"
+      url "https://github.com/faramesh/faramesh-core/releases/download/v#{version}/faramesh-linux-amd64"
       sha256 "818db1798daccd271ecd24416271c348caf796a4fb88e8eb890496708e4e4b2a"
     end
   end
@@ -46,7 +46,7 @@ class Faramesh < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("\#{bin}/faramesh --version")
+    assert_match version.to_s, shell_output("#{bin}/faramesh --version")
     system bin/"faramesh", "demo"
   end
 end
